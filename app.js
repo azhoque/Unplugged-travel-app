201d9b46869c94e80e1ad0e86d6bfbd6ec731f5c
Azizi <azizi@thinkful.com>
2017-09-18 03:11:22 -0500

    Added google map

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function getDataFromApi(searchTerm, callback) {
     url: travelSiteApi,
     data: {
       query: `${searchTerm}`,
-      limit: 6,
+      limit: 8,
     },
     headers: {
       "x-api-key": "lEoKPbust75vnILH1Ue5g34x301P6UK89wytadmk"
@@ -19,114 +19,104 @@ function getDataFromApi(searchTerm, callback) {
   $.ajax(apiCall);
 }
 
-function getWeatherApi(searchTerm, callback){
-  const weatherApi = 'http://api.openweathermap.org/data/2.5/weather?q=' + searchTerm +"&units=Imperial" + "&APPID=7b87148d8f8b76f5a6623a78f3de3152";
+function getWeatherApi(searchTerm, callback) {
+  const weatherApi = 'http://api.openweathermap.org/data/2.5/weather?q=' + searchTerm + "&units=Imperial" + "&APPID=7b87148d8f8b76f5a6623a78f3de3152";
   const api2Call = {
     url: weatherApi,
     data: {
       query: `${searchTerm}`,
-      },
-      dataType: "json",
-      type: "GET",
-      success: callback
-    };
+    },
+    dataType: "json",
+    type: "GET",
+    success: callback
+  };
 
-    $.ajax(api2Call);
-  }
-  function getMapApi(searchTerm, callback){
-    const mapApi = 'https://maps.googleapis.com/maps/api/js?query='+ searchTerm +'&key=AIzaSyDWIyUQOV2Ud4rxM4H-hEC6kph2XjxhV_A';
-    const api3Call = {
-      url: mapApi,
-      data: {
-        query: `${searchTerm}`,
-      },
-        dataType: "json",
-        type: "GET",
-        success: callback
-      };
+  $.ajax(api2Call);
+}
 
-      $.ajax(api3Call);
+function initMap(data) {
+
+  var options = {
+    zoom: 8,
+    center: {
+      lat: 30.2672,
+      lng: -97.7431
     }
+  }
+  var map = new google.maps.Map(document.getElementById('map'), options);
+  var marker = new google.maps.Marker({
+    position: {
+      lat: 30.2672,
+      lng: -97.7431
+    },
+    map: map,
+  });
 
-
-    function initMap(data){
-
-      var options = {
-        zoom:8,
-        center: {lat:30.2672, lng:-97.7431}
-      }
-      var map = new google.maps.Map(document.getElementById('map'), options);
-      var marker = new google.maps.Marker({
-        position:{lat:30.2672, lng:-97.7431},
-        map:map,
-      });
-      }
+}
 
 
 function getOutput(out) {
-    var city = out.name;
-    var country = out.name_suffix;
-    var description = out.perex;
-    var thumb = out.thumbnail_url;
-    var url = "https://travel.sygic.com/#/?wizard&type=poi&end=2017-09-15&start=2017-09-15";
-   // var temp = out.weather.main;
-    var output =
-    `<div class="col s4">
+  var city = out.name;
+  var country = out.name_suffix;
+  var description = out.perex;
+  var thumb = out.thumbnail_url;
+  var url = "https://travel.sygic.com/#/?wizard&type=poi&end=2017-09-15&start=2017-09-15";
+  // var temp = out.weather.main;
+  var output =
+    `<div class="col s3">
           <div class="card small">
-          <div class="card">
        <div class="card-image waves-effect waves-block waves-light">
          <img class="activator" src=${thumb}>
        </div>
        <div class="card-content">
-         <span class="card-title activator grey-text text-darken-4">${city}, ${country}<i class="material-icons right">more_vert</i></span>
-
+         <span class="card-title activator grey-darken-4"><h6>${city}, ${country}</h6><i class="material-icons right">more_vert</i></span>
        </div>
        <div class="card-reveal">
-         <span class="card-title grey-text text-darken-4">${city}, ${country}<i class="material-icons right">close</i></span>
+         <span class="card-title grey-text text-darken-4 flow-text">${city}, ${country}<i class="material-icons right">close</i></span>
          <p>${description}</p><br>
          <a href="${url}" target=_blank>Learn more</a>
        </div>
      </div>
-          </div>
+
         </div>`;
 
-    return output;
-  }
+  return output;
+}
 
-  function getWeather(item, value) {
+function getWeather(item, value) {
 
-    var outputWeather = `<div>${item}: ${value}</div>`;
-    return outputWeather;
-  }
+  var outputWeather = `<div>${item}: ${value}</div>`;
+  return outputWeather;
+}
 
-  function displaySearchData(data) {
-    const results = data.data.places.map((item, index) => getOutput(item));
-    $('#js-results').html(results);
-    console.log(results);
-  }
+function displaySearchData(data) {
+  const results = data.data.places.map((item, index) => getOutput(item));
+  $('#js-results').html(results);
+  console.log(results);
+}
 
-  function displayWeatherData(data){
-    console.log(data);
-    const weatherResult = Object.keys(data.main).map((item) => getWeather(item, data.main[item]));
-    $('.tap-target-content').html(weatherResult);
-    console.log(weatherResult);
-  }
+function displayWeatherData(data) {
+  console.log(data);
+  const weatherResult = Object.keys(data.main).map((item) => getWeather(item, data.main[item]));
+  $('.tap-target-content').html(weatherResult);
+  console.log(weatherResult);
+}
 
 
 $(function() {
   $('.parallax').parallax();
-    $("#js-form").submit(event => {
-      event.preventDefault();
-      const queryTarget = $(event.currentTarget).find('#js-input');
-      const query = queryTarget.val();
-      $("#js-input").val("");
-   getDataFromApi(query, function(data){
-    console.log(data);
-    displaySearchData(data);
-   });
-    getWeatherApi(query, function(data){
+  $("#js-form").submit(event => {
+    event.preventDefault();
+    const queryTarget = $(event.currentTarget).find('#js-input');
+    const query = queryTarget.val();
+    $("#js-input").val("");
+    getDataFromApi(query, function(data) {
       console.log(data);
-     displayWeatherData(data);
+      displaySearchData(data);
     });
+    getWeatherApi(query, function(data) {
+      console.log(data);
+      displayWeatherData(data);
     });
   });
+});
